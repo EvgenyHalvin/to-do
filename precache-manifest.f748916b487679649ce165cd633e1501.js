a713ebcca5b8b1dacc78b180644e62ddccc2a908
Evgeny Zhdanov <evgeny1halvin@yandex.ru>
2021-07-08 02:13:46 +0200

    Updates

---
+++ precache-manifest.f748916b487679649ce165cd633e1501.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53478f09fb3f750b81135fe94a8a3dad",
+    "url": "/EvgenyHalvin/to-do/index.html"
+  },
+  {
+    "revision": "2772f5e8ccf5400ee389",
+    "url": "/EvgenyHalvin/to-do/static/css/main.e1a80560.chunk.css"
+  },
+  {
+    "revision": "11b8f7f5d3b53cbc0f3d",
+    "url": "/EvgenyHalvin/to-do/static/js/2.b04c78c9.chunk.js"
+  },
+  {
+    "revision": "2772f5e8ccf5400ee389",
+    "url": "/EvgenyHalvin/to-do/static/js/main.95b3b251.chunk.js"
+  },
+  {
+    "revision": "4361643ce14d03c3c90d",
+    "url": "/EvgenyHalvin/to-do/static/js/runtime-main.5bca3446.js"
+  },
+  {
+    "revision": "14a450a3d2fd191fcefa23b273baaf14",
+    "url": "/EvgenyHalvin/to-do/static/media/Inter-Black.14a450a3.woff"
+  },
+  {
+    "revision": "e11ae7208ed88b29513e6f9652113d46",
+    "url": "/EvgenyHalvin/to-do/static/media/Inter-Black.e11ae720.woff2"
+  },
+  {
+    "revision": "46d7404e4a2eb3cd2ff9bc83023e259c",
+    "url": "/EvgenyHalvin/to-do/static/media/Inter-Medium.46d7404e.woff2"
+  },
+  {
+    "revision": "5ce3e4db9634913232403f166b2447de",
+    "url": "/EvgenyHalvin/to-do/static/media/Inter-Medium.5ce3e4db.woff"
+  },
+  {
+    "revision": "14d1275c67676cc5d911232d0c890d97",
+    "url": "/EvgenyHalvin/to-do/static/media/Inter-Regular.14d1275c.woff"
+  },
+  {
+    "revision": "8070997696b1c91905fecba458f96640",
+    "url": "/EvgenyHalvin/to-do/static/media/Inter-Regular.80709976.woff2"
+  },
+  {
+    "revision": "b64534713cde7b5902299eeb278b72e7",
+    "url": "/EvgenyHalvin/to-do/static/media/bin.b6453471.png"
+  },
+  {
+    "revision": "1b7a9b9838f6625769991cc3631d685f",
+    "url": "/EvgenyHalvin/to-do/static/media/save.1b7a9b98.png"
+  }
+]);
