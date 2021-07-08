6d214975786679fa0cba4be38a9ead71afd19851
Evgeny Zhdanov <evgeny1halvin@yandex.ru>
2021-07-08 02:18:49 +0200

    Updates

---
+++ precache-manifest.5a70e22be236c6c055f1221c0f949627.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ae58800b814d6edf5bd786b2760f96a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ec9522138164c03fc2b4",
+    "url": "./static/css/main.f191a9e9.chunk.css"
+  },
+  {
+    "revision": "11b8f7f5d3b53cbc0f3d",
+    "url": "./static/js/2.b04c78c9.chunk.js"
+  },
+  {
+    "revision": "ec9522138164c03fc2b4",
+    "url": "./static/js/main.d7cd5dfc.chunk.js"
+  },
+  {
+    "revision": "842d3dffebf8c2e8902b",
+    "url": "./static/js/runtime-main.8a172fa2.js"
+  },
+  {
+    "revision": "14a450a3d2fd191fcefa23b273baaf14",
+    "url": "./static/media/Inter-Black.14a450a3.woff"
+  },
+  {
+    "revision": "e11ae7208ed88b29513e6f9652113d46",
+    "url": "./static/media/Inter-Black.e11ae720.woff2"
+  },
+  {
+    "revision": "46d7404e4a2eb3cd2ff9bc83023e259c",
+    "url": "./static/media/Inter-Medium.46d7404e.woff2"
+  },
+  {
+    "revision": "5ce3e4db9634913232403f166b2447de",
+    "url": "./static/media/Inter-Medium.5ce3e4db.woff"
+  },
+  {
+    "revision": "14d1275c67676cc5d911232d0c890d97",
+    "url": "./static/media/Inter-Regular.14d1275c.woff"
+  },
+  {
+    "revision": "8070997696b1c91905fecba458f96640",
+    "url": "./static/media/Inter-Regular.80709976.woff2"
+  },
+  {
+    "revision": "b64534713cde7b5902299eeb278b72e7",
+    "url": "./static/media/bin.b6453471.png"
+  },
+  {
+    "revision": "1b7a9b9838f6625769991cc3631d685f",
+    "url": "./static/media/save.1b7a9b98.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/EvgenyHalvin/to-do/precache-manifest.f748916b487679649ce165cd633e1501.js"
+  "./precache-manifest.5a70e22be236c6c055f1221c0f949627.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/EvgenyHalvin/to-do/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
